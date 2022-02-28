6c0c5cd7f60065ac978daf10d436e23bd4bbb221
lodex23 <81414802+lodex23@users.noreply.github.com>
2022-02-28 17:35:18 +0100

    Update JeffTools.py

--- JeffTools.py
+++ JeffTools.py
@@ -5,49 +5,195 @@ os.system('clear')
 
 
 print("------------------------------------------")
-print("1) aircrack-ng")
-print("2) hydra")
-print("3) nikto")
-print("4) nmap")
+print("1) airscript")
+print("2) cracker   | SSH")
+print("3) AMFinder")
+print("4) nmapper")
 print("------------------------------------------")
 
 tool = input("Enter the tool u want to use(ctrl + C to exit): ")
 
 
 
-def Function_Aircrack():
-    os.system('python3 ---Path to aircrack-ng file---')
+def Function_airscript():
+    os.system('airmon-ng check kill')
+    os.system('clear')
+
+    print("Automated Aircrack-ng tool By Jeffrey")
+    print("-------------------------------------------")
+
+    interface = input("Type interface: ")
+    print("-------------------------------------------")
+    print("Interface set to: " + interface)
+    print("-------------------------------------------")
+    input("Press Enter to continue")
+
+    os.system('clear')
+
+    input("Press Enter to start monitor mode")
+    print("-------------------------------------------")
+
+    os.system('airmon-ng start ' + interface)
+
+    print("-------------------------------------------")
+    input("Press enter to continue")
 
 
-def Function_hydra():
-    os.system('python3 ---Path to hydra file---')
-
-
-def Function_nikto():
-    os.system('python3 ---Path to nikto file---')
-
-
-def Function_nmap():
-    os.system('python3 ---Path to nmap file---')
+    os.system('airodump-ng ' + interface + 'mon')
+    input("Press enter to continue")
 
 
 
+    os.system('clear')
 
 
-if tool = "1":
-    Function_Aircrack()
+    os.system('airodump-ng --bssid ' + bssid + ' -c ' + channel + ' --write WPAcrack mon0')
+    print("-------------------------------------------")
+    input("Press enter to continue")
 
-elif tool = "2":
-    Function_hydra()
+    deauth = input("Enter the number of de-authenticate frames you want to send (example: 100): ")
 
+    os.system('aireplay-ng --deauth ' + deauth + ' -a ' + bssid + ' mon0')
 
-elif tool = "3":
-    Function_nikto()
-
-
-elif tool = "4":
-    Function_nmap()
+    os.system('JeffTools')
 
 
-else:
-    print('This number is invalid.')
+
+def Function_cracker_ssh():
+    import os
+
+
+    def ssh(wrdList, Host):
+          os.system('hydra -L ' + wrdList + ' -P ' + wrdList + ' ' + Host + ' ssh')
+
+
+
+
+    path = input("Enter the path of the Wordlist: ")
+    print("------------------------------------------")
+    print("Path set to: " + path)
+    print("------------------------------------------")
+    input("Press enter to continue")
+
+    os.system('clear')
+
+    ip = input("Enter Target ip: ")
+    print("------------------------------------------")
+    print("Set ip to: " + ip)
+    print("------------------------------------------")
+    input("Press enter to start attack")
+
+    os.system('clear')
+
+
+    ssh(path, ip)
+
+    os.system('JeffTools')
+
+
+
+def Function_AMFinder():
+    import os
+
+
+    os.system('clear')
+
+
+    print("----------------------------------------------------------------")
+    print("|  Welcome to AdminFinder an automated nikto tool by Jeffrey   |")
+    print("----------------------------------------------------------------")
+
+    url = input("Enter Host/url: ")
+
+    os.system('clear')
+
+
+    print("----------------------------------------------------------------")
+    print("|  Welcome to AdminFinder an automated nikto tool by Jeffrey   |")
+    print("----------------------------------------------------------------")
+    print("----------------------------------------------------------------")
+    print("|  Set host to: " + url)
+    print("----------------------------------------------------------------")
+
+    input("Press enter to start scanning: " + url)
+
+
+    os.system('clear')
+
+    os.system('nikto -host ' + url)
+    os.system('JeffTools')
+
+
+def Function_nmapper():
+    import os
+
+    os.system('clear')
+
+
+    ip = input("Enter Target ip(Ctrl + c to exit): ")
+    os.system('clear')
+    print("-----------------------------")
+    print("Target Ip set to: " + ip)
+    print("-----------------------------")
+
+    os.system('clear')
+
+
+
+    def Function_IP_protocol(host):
+        os.system('nmap -sO ' + host)
+        input("Press enter to go back")
+        os.system('nmapper')
+
+
+    def Function_Service_Version(host):
+        os.system('nmap -sV ' + host)
+        input("Press enter to go back")
+        os.system('nmapper')
+
+    def Function_All_Hosts(host):
+        os.system('nmap -sV ' + host + ' -Pn')
+        os.system("Press enter to go back")
+        os.system('nmapper')
+
+
+    def Function_Exit():
+    	os.system('exit')
+
+
+
+
+    print("Ip: " + ip)
+    print("--------------------------------")
+    print("Choose option")
+    print("--------------------------------")
+    print("1) Ip Protocol")
+    print("2) Service/Version")
+    print("3) All Hosts (slow)")
+    print("4) Exit")
+    print("--------------------------------")
+
+    oP = input("Choose option: ")
+
+
+    os.system('clear')
+
+
+
+    if oP == "1":
+        Function_IP_protocol(ip)
+
+    elif oP == "2":
+        Function_Service_Version(ip)
+
+    elif oP == "3":
+        Function_All_Hosts(ip)
+
+    elif oP == "4":
+        Function_Exit()
+
+    else:
+        print("This Number is invalid.")
+
+
+    os.system('JeffTools')

